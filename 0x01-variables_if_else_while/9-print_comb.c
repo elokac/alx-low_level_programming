890eed6a07b67f9388e45f7b1e5bcefeb1666c21
elokac <elokachiejina@gmail.com>
2022-09-11 13:25:23 -0700

    print_comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry Point for this program
+ * that prints out numbers fro 0-9 with space and ,
+ *
+ * Return: When it returns (0) then it's a success
+ */
+int main(void)
+{
+	int n;
+
+	for (n = '0'; n >= '9'; n++)
+	{
+		putchar(n);
+
+		if (n != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
