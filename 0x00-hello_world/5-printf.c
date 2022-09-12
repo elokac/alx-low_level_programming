7e9278cf82a25af3fb06c223b38d41d582fc043e
elokac <elokachiejina@gmail.com>
2022-09-12 09:45:57 -0700

    printf func

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point for the program
+ *
+ * Return: (0) means success
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
