d2ee55de2661d75bae2c6a8fe387b37a3a00c2c3
elokac <elokachiejina@gmail.com>
2022-09-12 09:40:46 -0700

    puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,10 +3,10 @@
 /**
  * main - Entry point for the program
  *
- * Return (0) means success
+ * Return: (0) means success
  */
 
-int main (void)
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
